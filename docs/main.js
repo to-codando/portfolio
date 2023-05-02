54cfad188430c251ea4e7be358d093321dd01d5f
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2023-05-02 17:04:51 -0300

    feat: update docs

--- docs/main.js
+++ docs/main.js
@@ -1230,8 +1230,8 @@ var template3 = ({ actions }) => html`
           <${AppContent}>
             <slot target="content" ctx="app-home">
               <div class="wrap-banner-ctx hide-video" onClick=${actions.showVideo}>
-                <${tt} src="/assets/images/bg-banner-video.png" fallbackSrc="/assets/images/fallback-black.png" alt="A ajuda que você procura pode estar aqui"/>
-                <${tt} src="/assets/images/bg-banner-responsive.png" fallbackSrc="/assets/images/fallback-black.png" alt="A ajuda que você procura pode estar aqui"/>
+                <${tt} src="https://user-images.githubusercontent.com/94268593/235769500-6b12101b-0b2e-4f5a-9f9c-12275e5c7baa.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="A ajuda que você procura pode estar aqui"/>
+                <${tt} src="https://user-images.githubusercontent.com/94268593/235769495-af997379-8e70-4627-8d70-ef54ba60a0f6.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="A ajuda que você procura pode estar aqui"/>
                 <div class="wrap-video-ctx">
                   <iframe  src="https://www.youtube-nocookie.com/embed/Q5bjmh9TzlU?rel=0&amp;controls=0&amp&amp;showinfo=0&amp;modestbranding=1" frameborder="0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"></iframe>                
                 </div>
@@ -1285,43 +1285,43 @@ var template3 = ({ actions }) => html`
                 <ul class="wrap-logo-ctx">
                   <li>
                     <a href="#/angular" target="_blank">
-                      <${tt} src="/assets/images/logo-node-js.png" fallbackSrc="/assets/images/fallback-black.png" alt="Node JS"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769558-5019eb94-17b8-4c20-b074-b094c6974658.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Node JS"/>
                       <span>Node JS</span>
                     </a>
                   </li>
                   <li>
                     <a href="#/angular" target="_blank">
-                      <${tt} src="/assets/images/logo-next-js.png" fallbackSrc="/assets/images/fallback-black.png" alt="Next"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769555-19d579b2-03e2-42d7-845b-7633d9074732.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Next"/>
                       <span>Next JS</span>
                     </a>
                   </li>
                   <li>
                     <a href="#/angular" target="_blank">
-                      <${tt} src="/assets/images/logo-tailwind-css.png" fallbackSrc="/assets/images/fallback-black.png" alt="Tailwind"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769609-db83daf3-1cdd-4e92-922b-a52d1225582b.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Tailwind"/>
                       <span>Tailwind CSS</span>
                     </a>
                   </li>
                   <li>
                     <a href="#/angular" target="_blank">
-                      <${tt} src="/assets/images/logo-react-js.png" fallbackSrc="/assets/images/fallback-black.png" alt="React"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769593-42f4abcc-87eb-42b1-8020-e66ae7a930b6.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="React"/>
                       <span>React</span>
                     </a>
                   </li>
                   <li>
                     <a href="#/angular" target="_blank">
-                      <${tt} src="/assets/images/logo-angular.png" fallbackSrc="/assets/images/fallback-black.png" alt="Angular"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769518-bf383d33-048e-4558-802a-2e494951e659.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Angular"/>
                       <span>Angular</span>
                     </a>
                   </li>
                   <li>
                     <a href="#/angular" target="_blank">
-                      <${tt} src="/assets/images/logo-vue-js.png" fallbackSrc="/assets/images/fallback-black.png" alt="Vue"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769611-54917257-0d97-4221-80b7-8e836cb564c0.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Vue"/>
                       <span>Vue</span>
                     </a>
                   </li>
                   <li>
                     <a href="#/angular" target="_blank">
-                      <${tt} src="/assets/images/logo-capacitor.png" fallbackSrc="/assets/images/fallback-black.png" alt="Capacitor Framework"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769528-7d9d4a65-22a4-4dae-b5d2-91ca3fa98065.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Capacitor Framework"/>
                       <span>Capacitor</span>
                     </a>
                   </li>
@@ -1375,18 +1375,18 @@ var template3 = ({ actions }) => html`
             <slot target="content" ctx="app-home">
                 <ul class="wrap-logo-ctx section-amount">
                   <li>
-                    <a href="https://www.npmjs.com/package/iares" target="_blank">
-                      <${tt} src="/assets/images/logo-npm.png" fallbackSrc="/assets/images/fallback-black.png" alt="IARES no NPM"/>
+                    <a href="https://user-images.githubusercontent.com/94268593/235769561-52d3057c-9d85-4c25-8083-5a3f36128633.png" target="_blank">
+                      <${tt} src="/assets/images/logo-npm.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="IARES no NPM"/>
                     </a>
                   </li>
                   <li>
-                    <a href="https://github.com/to-codando/iares-ui" target="_blank">
-                      <${tt} src="/assets/images/logo-iares.png" fallbackSrc="/assets/images/fallback-black.png" alt="IARES no Github"/>
+                    <a href="https://user-images.githubusercontent.com/94268593/235769538-a14ea12b-4615-42c0-8a43-806f46e501b5.png" target="_blank">
+                      <${tt} src="/assets/images/logo-iares.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="IARES no Github"/>
                     </a>
                   </li>
                   <li>
-                    <a href="https://to-codando.github.io/fam-code-ui/" target="_blank">
-                      <${tt} src="/assets/images/logo-iares-ui.png" fallbackSrc="/assets/images/fallback-black.png" alt="IARES UI biblioteca de componentes"/>
+                    <a href="https://user-images.githubusercontent.com/94268593/235769542-ef462a30-e10d-4d09-aebd-36efad24ea0b.png" target="_blank">
+                      <${tt} src="/assets/images/logo-iares-ui.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="IARES UI biblioteca de componentes"/>
                     </a>
                   </li>
                 </ul>
@@ -1439,34 +1439,34 @@ var template3 = ({ actions }) => html`
             <slot target="content" ctx="app-home">
                 <ul class="wrap-logo-ctx section-amount">
                   <li>
-                      <${tt} src="/assets/images/logo-ifood.png" fallbackSrc="/assets/images/fallback-black.png" alt="Ifood"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769550-3878f174-db65-4542-99ae-b663891591d4.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Ifood"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-samsung.png" fallbackSrc="/assets/images/fallback-black.png" alt="Samsung"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769601-59679148-d4c6-4b04-b647-d57706d363bd.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Samsung"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-hotmart.png" fallbackSrc="/assets/images/fallback-black.png" alt="Hotmart"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769536-496dddbb-d823-4dc1-8c7e-414c00c0b46a.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Hotmart"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-crefisa.png" fallbackSrc="/assets/images/fallback-black.png" alt="Crefisa"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769529-453a14b4-64a1-4a1f-9c1c-874b860e67f4.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Crefisa"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-proz.png" fallbackSrc="/assets/images/fallback-black.png" alt="Proz"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769588-abb53366-77d8-4e9b-8b1b-624ce5fb7169.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Proz"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-pede-pronto.png" fallbackSrc="/assets/images/fallback-black.png" alt="Pede Pronto"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769577-78c67353-1c08-4098-a710-66189ba67095.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Pede Pronto"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-bs2.png" fallbackSrc="/assets/images/fallback-black.png" alt="Banco BS2"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769526-6ac9da55-2d7d-46db-a042-c870ab32af1c.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Banco BS2"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-beetech.png" fallbackSrc="/assets/images/fallback-black.png" alt="Beetech"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769522-886ac28e-dfd1-45e1-bc92-296b81fb0015.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Beetech"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-riachuelo.png" fallbackSrc="/assets/images/fallback-black.png" alt="Riachuelo"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769595-66f2ee01-2597-46bd-8054-e393c065dba5.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Riachuelo"/>
                   </li>
                   <li>
-                      <${tt} src="/assets/images/logo-porto-seguro.png" fallbackSrc="/assets/images/fallback-black.png" alt="Porto Seguro"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769584-681005f4-4a1d-4c26-9678-b961387e2d87.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Porto Seguro"/>
                   </li>
                 </ul>
             </slot>
@@ -1518,25 +1518,25 @@ var template3 = ({ actions }) => html`
                 <ul class="wrap-logo-ctx wrap-social">
                   <li>
                     <a href="https://github.com/to-codando/" target="_blank">
-                      <${tt} src="/assets/images/logo-github.png" fallbackSrc="/assets/images/fallback-black.png" alt="Github"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769532-f745ba99-d53d-4cff-b3eb-886040c8815d.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Github"/>
                       <span>Github</span>
                     </a>
                   </li>
                   <li>
                     <a href="https://www.linkedin.com/in/rodrigo-lima-rocha-59006089/" target="_blank">
-                      <${tt} src="/assets/images/logo-linkedin.png" fallbackSrc="/assets/images/fallback-black.png" alt="Linkedin"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769553-a46a3a8b-9003-499b-937a-b24b77686639.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Linkedin"/>
                       <span>Linkedin</span>
                     </a>
                   </li>
                   <li>
                     <a href="https://www.youtube.com/channel/UCdJEUSVsNHHXb5ut3nRizng" target="_blank">
-                      <${tt} src="/assets/images/logo-youtube.png" fallbackSrc="/assets/images/fallback-black.png" alt="Youtube"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769614-3390f8cc-7f02-469c-b627-c5810cd2d6c5.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Youtube"/>
                       <span>Youtube</span>
                     </a>
                   </li>
                   <li>
                     <a href="https://www.tabnews.com.br/tocodando" target="_blank">
-                      <${tt} src="/assets/images/logo-tabnews.png" fallbackSrc="/assets/images/fallback-black.png" alt="Blog"/>
+                      <${tt} src="https://user-images.githubusercontent.com/94268593/235769606-ff0a3a2b-06d0-46af-9bf1-ce79df825a61.png" fallbackSrc="https://user-images.githubusercontent.com/94268593/235769510-b205a491-7e75-4aac-a1fd-06ae855bae76.png" alt="Blog"/>
                       <span>Blog</span>
                     </a>
                   </li>
